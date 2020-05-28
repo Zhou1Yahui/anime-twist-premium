494be8bcadec4270ed98eccded6351754f553e6b
thoughtsunificator <oss@thoughtsunificator.me>
2020-05-29 00:53:14 +0200

    removed logs

--- object/search.js
+++ object/search.js
@@ -9,7 +9,7 @@ ATP.Search = function() {
  * @return {SearchEntry}
  */
 ATP.Search.prototype.addEntry = function(data) {
-	console.log("[atp] Adding a new search entry...")
+	ATP.log("[atp] Adding a new search entry...", data)
 	const entry = new ATP.SearchEntry(data.title, data.slug)
 	this.entries.push(entry)
 	return entry
@@ -17,8 +17,6 @@ ATP.Search.prototype.addEntry = function(data) {
 
 ATP.Search.prototype.getRandomEntry = function() {
 	const entries = this.entries.filter(entry => !ATP.watchList.entries.find(entry_ => entry_.slug === entry.slug))
-	console.log(this.entries)
-	console.log(entries.length)
 	return entries[Math.floor(Math.random()*this.entries.length)]
 }
 
